7cf44874d939fc821e5488b981cc92edf43778b8
Yaseen Lahmami <33607275+Youssless@users.noreply.github.com>
2019-06-19 15:06:01 +0100

    checks if the user is in game

---
+++ RiotApiConst.py
@@ -0,0 +1,12 @@
+RGAPI_URL = {
+    "summoner":"{proxy}/lol/summoner/{version}/summoners/by-name/{ign}?api_key={api_key}",
+    "spectator":"{proxy}/lol/spectator/{version}/active-games/by-summoner/{user_id}?api_key={api_key}"
+}
+
+TOKENS = {
+    "proxy":"https://euw1.api.riotgames.com",
+    "version":"v4",
+    "ign":"Youssless",
+    "api_key":"RGAPI-f4ecb915-e362-4ab8-a988-481bca2b219c",
+    "user_id":"{user_id}"
+}
--- click.py
+++ click.py
@@ -1,16 +1,18 @@
 from pynput.mouse import Listener
+count=0
 
-count = 0
-
-#f = open("num_clicks.log", "a")
-
+# function that handles when the user clicks on the mouse
 def on_click(x, y, button, pressed):
     global count
     if pressed:
-        count = count+1
+        count=count+1
+        # write the clicks to a file
         with open("num_clicks.txt", "w") as f:
             f.write(str(count))
 
-with Listener(on_click = on_click) as listener:
-    listener.join()
+def listen():
+    with Listener(on_click = on_click) as listener:
+        listener.join()
+    
+
 
