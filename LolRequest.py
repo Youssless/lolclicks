8a870ac2f0092b540245423a13983d59a4211650
Yaseen Lahmami <33607275+Youssless@users.noreply.github.com>
2019-07-12 18:41:42 +0100

    bug when game ends

--- LolRequest.py
+++ LolRequest.py
@@ -1,101 +1,105 @@
 import requests
 import json
 import time
+from pynput.mouse import Listener
+import threading
+import datetime as Date
 
 class GenerateRequest:
-
-
-    def __init__(self, ref):
+    def __init__(self, ign):
         self.RGAPI_URL = {
-            'rgapiurl':'{proxy}/{category}/{version}/{category_by}/{ref}?api_key={api_key}',
-            #"summoner":"{proxy}/lol/summoner/{version}/summoners/by-name/{ign}?api_key={api_key}",
-            #"spectator":"{proxy}/lol/spectator/{version}/active-games/by-summoner/{user_id}?api_key={api_key}"
+            "summoner":"{proxy}/lol/summoner/{version}/summoners/by-name/{ign}?api_key={api_key}",
+            "spectator":"{proxy}/lol/spectator/{version}/active-games/by-summoner/{user_id}?api_key={api_key}"
         }
 
         with open("apikey.json") as f:
             k = json.load(f)
 
-        self.proxy = 'https://euw1.api.riotgames.com'
-        self.category = 'lol/summoner'
-        self.version = 'v4'
-        self.category_by = 'summoners/by-name'
-        self.ref = ref
-        self.api_key = k["key"]
+        self.ign = ign
+        self.api_key=k["key"]
+        self.summoner_url = self.__build_url('summoner')
+        self.spectator_url = self.__build_url('spectator')
 
-        self.summoner_url = self.__build_url()
-        self.spectator_url = None
-
-    def __build_url(self):
-        url = self.RGAPI_URL['rgapiurl'].format(
-            proxy=self.proxy,
-            category=self.category,
-            version=self.version,
-            category_by=self.category_by,
-            ref=self.ref,
+    def __build_url(self, t):
+        url = self.RGAPI_URL[t].format(
+            proxy="https://euw1.api.riotgames.com",
+            version="v4",
+            ign=self.ign if t=='summoner' else None,
+            user_id=self.request_id() if t=='spectator' else None,
             api_key=self.api_key
         )
-
         return url
 
-    '''
     def request_id(self):
         summoner_json = requests.get(self.summoner_url)
         return summoner_json.json()['id']
-    '''
 
     def request_status_code(self):
-        self.set_spectator()
         spectator_json = requests.get(self.spectator_url)
-        time.sleep(5)
+        print(self.summoner_url)
+        print(self.spectator_url)
         return spectator_json.status_code
 
-    def request_json(self):
-        summoner_json = requests.get(self.summoner_url)
-        return summoner_json.json()
+    def request_json(self, t):
+        if t == 'summoner':
+            summoner_json = requests.get(self.summoner_url)
+            return summoner_json.json()
+        elif t == 'spectator':
+            spectator_json = requests.get(self.spectator_url)
+            return spectator_json
 
-    def set_spectator(self):
-        self.category = 'lol/summoner'
-        self.category_by = 'active-games/by-summoner'
-        self.ref = 'uGSlgOV2k_r---8-a1YdjXWbCzI0yfNUj-dSWqUpzR8j5k4'
-        #self.request_json()['id']
-
-        self.spectator_url = self.__build_url()
 
 class LoLRequest:
-    def __init__(self, ign):
+    def __init__(self, ign, listener):
         self.ign = ign
         self.generate_request = GenerateRequest(self.ign)
+        self.listener = listener
 
-    def handle_request(self, listener):
+    def handle_request(self):
         code = self.generate_request.request_status_code()
+        handler = threading.Thread(target=self.check_summoner_status, args=(code,))
         print(code)
 
         if code == 200:
-            checker = self.check_summoner_status(code)
-            if checker == 404:
-                listener.stop()
+            handler.start()
+            #self.check_summoner_status(listener, code)
         elif code == 404:
             print("{0} is currently not in game <{1}>".format(self.ign, code))
             print("Terminating ... ")
-            listener.stop()
+            self.listener.stop()
         elif code == 403:
             print(self.generate_request.summoner_url)
             print(self.generate_request.spectator_url)
-            print(self.generate_request.request_json())
-            listener.stop()
+            print(self.generate_request.request_json('spectator'))
+            self.listener.stop()
 
     def check_summoner_status(self, code):
+        c = 0
         while code == 200:
             if code == 404:
-                return code
+                self.listener.stop()
+                print("{0} is currently not in game <{1}>".format(self.ign, code))
+
+                curr_time = Date.datetime.now()
+                with open("num_clicks.log", "a") as f:
+                    f.write(curr_time.strftime("[%d/%m/%Y %H:%M:%S] ") + " clicks")
+                    f.write("\n")
+                exit()
+                break
             elif code == 500:
-                return code
+                self.listener.stop()
+                break
             elif code == 503:
-                return code
+                self.listener.stop()
+                break
 
             code = self.generate_request.request_status_code()
             print("{0} is currently in game <{1}>".format(self.ign, code))
+            c=c+1
+            print(str(c))
+            time.sleep(5)
 
+    
             
             
 
--- Main.py
+++ Main.py
@@ -1,27 +1,20 @@
 from LolRequest import LoLRequest
 from pynput.mouse import Listener
 import threading
-import datetime as Date
+
+
 
 count = 0
-
 def main():
-        #ign = input ('LoL Username: ')
-        lolr = LoLRequest('Youssless')
-        try:
-        # listen for clicks when the game starts
-                with Listener(on_click=on_click) as listener:
-                # Thread to handle checking if the game has ended
-                # While the main thread handles the click listener
-                        handler = threading.Thread(target=lolr.handle_request, args=(listener,))
-                        handler.start()
-                        listener.join()        
-        except KeyError:
-                print("API key has expired or cannot be found, generate at:" 
-                +" https://developer.riotgames.com/")
+    ign = input ('[EUW1] LoL Username: ')
+    
+    with Listener(on_click=on_click) as listener:
+        lolr = LoLRequest(ign, listener)
+        lolr.handle_request()
+        listener.join()
+        
 
 
-# function that handles when the user clicks on the mouse and counts them
 def on_click(x, y, button, pressed):
     global count
     if pressed:
