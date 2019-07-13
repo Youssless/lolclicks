f9f32d7a31f119c90277212d3a6b9961e17c8225
Yaseen Lahmami <33607275+Youssless@users.noreply.github.com>
2019-07-13 13:11:04 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,16 +2,18 @@ from LolRequest import LoLRequest
 from pynput.mouse import Listener
 import threading
 
+
 ign = input ('[EUW1] LoL Username: ')
 lolr = LoLRequest(ign, None)
+# thread for printing the check while mouse listener runs on a seperate thread
 handler = threading.Thread(target=lolr.handle_request)
 
-
-
+# event for clicking a mouse button
 def on_click(x, y, button, pressed):
     if pressed:
         lolr.count = lolr.count + 1
 
+# listens for the event
 with Listener(on_click=on_click) as listener:
     lolr.listener = listener
     handler.start()
